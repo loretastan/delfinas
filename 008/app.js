c08daa97688908b3ccd6f51ff39a873b8953bd55
loretastan <lloretaa1234@gmail.com>
2023-11-14 13:30:24 +0200

    js

---
+++ 008/app.js
@@ -0,0 +1,103 @@
+console.log('Hello, Racoons, from app js...');
+
+
+// let box1 = 'Green Apple';
+// let box2 = 'Red Apple';
+// let box3 = 48;
+// let box4 = '48';
+
+// let bigBox1 = box3 * box3;
+
+
+// let bigBox2 = box1 * box2;
+
+// console.log(bigBox2, typeof bigBox2)
+
+// console.log(box1, typeof box1)
+// console.log(box3, typeof box3)
+
+
+// console.log('My boxes:', box1, box2, box3, box4);
+// console.log('My big box:', bigBox1, bigBox2)
+
+//let appleCount = 8;
+//let orangeCount = 6;
+//let milkCount = 2;
+
+//let applePrice = 0.99;
+//let orangePrice = 1.29;
+//let milkPrice = 2.99;
+
+//let appleName = 'Apple';
+//let orangeName = 'Orange';
+// let milkName = 'Milk';
+
+// let appleTotal = appleCount * applePrice;
+// let orangeTotal = orangeCount * orangePrice;
+// let milkTotal = milkCount * milkPrice;
+
+
+// let payTotal = appleTotal + orangeTotal + milkTotal;
+
+// console.log('Youre total is :$', payTotal);
+
+// let whatIsThis = 100/ 0;
+
+// console.log(whatIsThis, typeof whatIsThis);
+
+// let whatIsThis2 = 0 / 0;
+
+// console.log(whatIsThis2, typeof whatIsThis2);
+
+// let whatIsThis3 = 0 / 100;
+
+// console.log(whatIsThis3, typeof whatIsThis3);
+
+// let whatIsThis4 = 0.1 + 0.2;
+
+// console.log(whatIsThis4, typeof whatIsThis4);
+
+// let whatIsThis5 = 0.1 + 0.2 + 0.3;
+
+// console.log(whatIsThis5, typeof whatIsThis5);
+
+
+// undefined => to number => NaN
+// undefined => to string => 'undefined'
+
+
+// let t1 = 'A' + 0;
+// console.log(t1, typeof t1);
+// let t2 = 'A' - 0;
+// console.log(t2, typeof t2);
+// let t3 = 'A1' - 0;
+// console.log(t3, typeof t3);
+// let t4 = '1A' - 0;
+// console.log(t4, typeof t4);
+// let t5 = '1' - 0;
+// console.log(t5, typeof t5);
+// let t6 = '' - 5;
+// console.log(t6, typeof t6);
+
+
+
+
+
+// let C = A + B;
+// console.log(C, typeof C);
+// C = A - B;
+// console.log(C, typeof C);
+// C = B % A;
+// console.log(C, typeof C);
+
+// A++;
+//A--;
+
+let A = 5;
+let B = 3;
+
+let C = A++ + ++A;
+console.log(C, typeof C);
+
+console.log('A', A);
+
