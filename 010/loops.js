3a59c1d6f06909c693bfe49155fd6d9a9d3d16ec
loretastan <lloretaa1234@gmail.com>
2023-11-17 15:31:05 +0200

    nd

--- 010/loops.js
+++ 010/loops.js
@@ -1,41 +1,36 @@
-console.log('Welcome to loops js. Have fun');
+console.log("Welcome to loops js. Have fun");
 function rand(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min +1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 // console.log('Hello, Raccon!');
 // console.log('Hello, Raccon!');
 // console.log('Hello, Raccon!');
 // console.log('Hello, Raccon!');
 // console.log('Hello, Raccon!');
-
 
 for (let i = 0; i < 5; i++) {
-
-    console.log('Hello, Raccon!', rand(0, 100));
-
+  console.log("Hello, Raccon!", rand(0, 100));
 }
 let coin;
- do {
-    coin = rand(0, 1) ? 'Heads' : 'Tails';
-    console.log('My coin is: ${coin} Nice!');
- } while(coin != 'Heads');
+do {
+  coin = rand(0, 1) ? "Heads" : "Tails";
+  console.log(`My coin is: ${coin} Nice!`);
+} while (coin != "Heads");
 
 //do {
-  //  var digit1 = rand(1, 20);
-    //var digit2 = rand(1, 20);
-    //console.log(digit1, digit2);
+//  var digit1 = rand(1, 20);
+//var digit2 = rand(1, 20);
+//console.log(digit1, digit2);
 //} while(digit1 <= 10 || digit2 <= 10);
 
 // > ---- <=
 // >= ---- <
 // == ---- !=
-// === ---- !== 
+// === ---- !==
 // && ---- ||
-
 
 let fiodor = 0;
 
@@ -46,7 +41,7 @@ fiodor += grandpa;
 console.log(`fiodor after birthday has ${fiodor} money`);
 
 while (fiodor < 300) {
-    let money = rand(50, 200);
-    fiodor += money;
-    console.log(`fiodor has ${fiodor} money`);
+  let money = rand(50, 200);
+  fiodor += money;
+  console.log(`fiodor has ${fiodor} money`);
 }
