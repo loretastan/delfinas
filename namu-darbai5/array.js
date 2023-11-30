b421d47788c10e34f6d9bf9ee9fdcf8cbcebfd5c
loretastan <lloretaa1234@gmail.com>
2023-11-30 14:47:56 +0200

    ggg

---
+++ namu-darbai5/array.js
@@ -0,0 +1,22 @@
+console.log('1.salyga');
+
+function spausdintiTeksta(tekstas) {
+console.log(tekstas);
+}
+spausdintiTeksta('tekstas');
+
+console.log('2.Salyga.');
+
+function spausdintiTekstaKartus(tekstas, kartai) {
+    for (let i = 0; i < kartai; i++) {
+        console.log(tekstas);
+    }
+}
+spausdintiTekstaKartus('Mano tekstas', 3);
+
+console.log('4.Salyga');
+
+
+
+
+
