73fdcba89180c2307dc36c721d8b4a316e7ea839
loretastan <lloretaa1234@gmail.com>
2023-11-30 18:20:14 +0200

    aaa

--- namu-darbai4/array.js
+++ namu-darbai4/array.js
@@ -28,6 +28,28 @@ let rezultatas =
 beLiekanos(skaicius);
 console.log(`Skaicius ${skaicius} dalijasi be liekanos is ${rezultatas} sveiku skaiciu.`);
 
+console.log('5.Salyga');
+
+function skaiciaiBeLiekanos(n) {
+    let dalijimai = 0;
+    for (let i = 2; i < n; i++)
+    if (n % i === 0) dalijimai++;
+    return dalijimai; 
+}
+
+const arr = Array.from({ length: 100}, () => Math.floor(Math.random() * 45) + 33);
+
+//arr.sort((a, b) => skaiciaiBeLiekanos(b) - skaiciaiBeLiekanos(a));
+
+const arr1 = arr.sort((a, b) => {
+    const dalijimaiA = skaiciaiBeLiekanos(a);
+    const dalijimaiB = skaiciaiBeLiekanos(b);
+    return dalijimaiA > dalijimaiB ? -1 : 1;
+});
+
+console.log(arr1)
+
+
 
 
 
